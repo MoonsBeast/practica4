eb9365b0868551988c6d4d824168e44af44a7bc0
MoonsBeast <r.calero.albarran@gmail.com>
2022-11-05 16:22:27 +0100

    Entrega

--- src/resolvers/get.ts
+++ src/resolvers/get.ts
@@ -16,16 +16,17 @@ export const getUser = async (context: GetUserContext) => {
         }
 
         let arg = {}
-        if (new RegExp("^[0-9]{8,8}[A-Za-z]$").test(context.params?.param)){
-            arg = { DNI: context.params?.param }
-        } else if (new RegExp("[0-9]{9}").test(context.params?.param)) {
-            arg = { telephone: context.params?.param }
-        } else if (new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$").test(context.params?.param)) {
+        if (new RegExp("^[0-9]{8,8}[A-Za-z]$").test(context.params.param)){
+            arg = { DNI: context.params.param }
+        } else if (new RegExp("[0-9]{9}").test(context.params.param)) {
+            arg = { telephone: context.params.param }
+        } else if (new RegExp("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$").test(context.params.param)) {
             arg = { email: context.params?.param }
-        } else if (new RegExp("([a-zA-Z]{2})\s*\t*(\d{2})\s*\t*(\d{4})\s*\t*(\d{4})\s*\t*(\d{2})\s*\t*(\d{10})").test(context.params?.param)) {
-            arg = { IBAN: context.params?.param }
+        } else if (new RegExp("([a-zA-Z]{2})\s*\t*(\d{2})\s*\t*(\d{4})\s*\t*(\d{4})\s*\t*(\d{2})\s*\t*(\d{10})","g").test(context.params.param)) {
+            /*Por alguna razon no entra aqui a pesar de que la expresion regular esta bien, la he probado en otros lugares y es correcta, así que no busca por iban*/
+            arg = { IBAN: context.params.param }
         } else {
-            arg = { _id: new ObjectId(context.params?.param)}
+            arg = { _id: new ObjectId(context.params.param)}
         }
 
         const user = await UserCollection.findOne(arg)
